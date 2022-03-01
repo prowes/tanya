36a64b67695b7a298c2c898ebe2f149ca4db4055
prowes <34424042+prowes@users.noreply.github.com>
2022-03-01 18:12:26 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+import tweepy
+import credentials  # please create your own
+
+#NB: you need to have Elevated twitter access to be able to post media
+auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
+auth.set_access_token(credentials.access_token, credentials.access_token_secret)
+
+api = tweepy.API(auth)
+filename = "pic.jfif"
+media = api.media_upload(filename)
+api.update_status(status='', media_ids=[media.media_id])
+
