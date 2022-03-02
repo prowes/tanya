13b1e4fa91fe8475ff52144aac468afc1267c7b5
prowes <34424042+prowes@users.noreply.github.com>
2022-03-02 18:59:27 +0200

    Update the file name for EC2

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret
 auth.set_access_token(credentials.access_token, credentials.access_token_secret)
 
 api = tweepy.API(auth)
-filename = "pic.jfif"
+filename = "/home/ec2-user/tanya/pic.jfif"
 media = api.media_upload(filename)
 api.update_status(status='', media_ids=[media.media_id])
 
